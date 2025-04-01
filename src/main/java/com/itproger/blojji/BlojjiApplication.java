c56bed7ccbe8a074edc52acad5d351d6f81371ec
eldar-05 <eldarzh123@gmail.com>
2025-04-01 11:39:31 +0600

    first commit
    Adding home page

---
+++ src/main/java/com/itproger/blojji/BlojjiApplication.java
@@ -0,0 +1,13 @@
+package com.itproger.blojji;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlojjiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlojjiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/itproger/blojji/BlojjiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itproger.blojji;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlojjiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
